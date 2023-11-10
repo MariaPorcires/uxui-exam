112929906f606f081d57f477a893ab8b0a4c77ab
hellnomary <maria.porcires@outlook.com>
2023-11-10 18:40:36 +0100

    pics

--- uxui-exam/src/views/Home/Home.jsx
+++ uxui-exam/src/views/Home/Home.jsx
@@ -4,13 +4,17 @@ import "./Home.css";
 function Home() {
   return (
     <main className="main">
-      <section className="main__section">
-        <h1 className="main__title">Photography by Mary</h1>
-      </section>
-      <section className="links">
-        <h2>About</h2>
-        <h2>About</h2>
-        <h2>About</h2>
+      <section className="main__background">
+        <section className="main__section">
+          <h1 className="main__title">Photography by Mary</h1>
+        </section>
+        <section className="flex">
+          <section className="links">
+            <button className="button">ABOUT</button>
+            <button className="button">PORTFOLIO</button>
+            <button className="button">CONTACT</button>
+          </section>
+        </section>
       </section>
     </main>
   );
