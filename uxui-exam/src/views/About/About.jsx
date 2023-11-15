d4d6fc5df4ab30ef404ea0cba8ee7e0b041c00a2
hellnomary <maria.porcires@outlook.com>
2023-11-15 10:50:36 +0100

    video

--- uxui-exam/src/views/About/About.jsx
+++ uxui-exam/src/views/About/About.jsx
@@ -3,10 +3,24 @@ import mary from "../../assets/mary.jpg";
 
 function About() {
   return (
-    <main>
-      <h1>Mary</h1>
-      <h2>Hobbyphotographer</h2>
-      <img className="about__img" src={mary} />
+    <main className="about">
+      <section className="about__container">
+        <section className="about__imgcontainer">
+          <video className="about__video" controls>
+            <source src="src\assets\video.mp4" type="video/mp4" />
+          </video>
+        </section>
+        <section className="about__text">
+          <h1 className="about__title">Mary</h1>
+          <h2>Hobbyphotographer</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui ex
+            voluptatum repudiandae velit quae est incidunt eos dolore distinctio
+            mollitia, nisi optio natus vel expedita ipsa quisquam ipsam
+            reiciendis minus.
+          </p>
+        </section>
+      </section>
     </main>
   );
 }
