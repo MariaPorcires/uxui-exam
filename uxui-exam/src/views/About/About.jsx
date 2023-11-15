a601ea159d98351fdad86e02ea4a2e9135667c83
hellnomary <maria.porcires@outlook.com>
2023-11-15 11:54:55 +0100

    about

--- uxui-exam/src/views/About/About.jsx
+++ uxui-exam/src/views/About/About.jsx
@@ -5,20 +5,23 @@ function About() {
   return (
     <main className="about">
       <section className="about__container">
+        <section className="about__text">
+          <h1 className="about__title">Mary</h1>
+          <h2 className="about__undertitle">Hobbyphotographer</h2>
+          <p className="about__paragraph">
+            I am Mary from Gothenburg, Sweden. I have always had a passion for
+            photography, ever since I was a child.<br></br>I have no education
+            in photography but have only had it as a hobby.<br></br>
+            <br></br>
+            If you want to know more about me and what I do, or if you need a
+            photographer, feel free to contact me and we can arrange something.
+          </p>
+        </section>
+
         <section className="about__imgcontainer">
           <video className="about__video" controls>
             <source src="src\assets\video.mp4" type="video/mp4" />
           </video>
-        </section>
-        <section className="about__text">
-          <h1 className="about__title">Mary</h1>
-          <h2>Hobbyphotographer</h2>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui ex
-            voluptatum repudiandae velit quae est incidunt eos dolore distinctio
-            mollitia, nisi optio natus vel expedita ipsa quisquam ipsam
-            reiciendis minus.
-          </p>
         </section>
       </section>
     </main>
